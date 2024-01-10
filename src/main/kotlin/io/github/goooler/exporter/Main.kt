250602351f6a03f9a92ad2249d68b2f712a24fdb
Goooler <wangzongler@gmail.com>
2024-01-11 02:42:18 +0800

    Input params

--- src/main/kotlin/io/github/goooler/exporter/Main.kt
+++ src/main/kotlin/io/github/goooler/exporter/Main.kt
@@ -2,15 +2,25 @@ package io.github.goooler.exporter
 
 import java.io.File
 import java.io.FileOutputStream
+import java.nio.file.Paths
+import kotlin.io.path.*
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.jdom2.Element
 import org.jdom2.input.SAXBuilder
 
-fun main() {
-  val folders = listOf(
-    "values",
-    "values-zh-rCN",
-  )
+fun main(args: Array<String>) {
+  val args = arrayOf("/Users/goooler/StudioProjects/lawnchair/res")
+
+  val errorMessage = "Please input the res folder path"
+  check(args.size == 1) {
+    errorMessage
+  }
+  val resFolder = Paths.get(args.first())
+  check(resFolder.exists() && resFolder.isDirectory()) {
+    errorMessage
+  }
+
+  val folders = resFolder.listDirectoryEntries("values*").sorted()
 
   val workbook = HSSFWorkbook()
   val sheet = workbook.createSheet("Sheet1")
@@ -22,16 +32,18 @@ fun main() {
   }
 
   folders.forEachIndexed { index, folder ->
-    val inputStream = StringRes::class.java.getResourceAsStream("/$folder/strings.xml")
+    val xmlFile = folder.resolve("strings.xml").takeIf { it.exists() } ?: return@forEachIndexed
+    val inputStream = xmlFile.inputStream()
     val elements = SAXBuilder().build(inputStream).rootElement.children
 
-    columns += if (folder == "values") {
+    val folderName = folder.name
+    columns += if (folderName == "values") {
       fillNewColumn(defaultColumn, elements)
     } else {
       val emptyColumn: StringResColumn = defaultColumn.mapValues { null }.toMutableMap()
       fillNewColumn(emptyColumn, elements)
     }
-    firstRow.createCell(index + 1).setCellValue(folder)
+    firstRow.createCell(index + 1).setCellValue(folderName)
   }
 
   columns.forEachIndexed { columnIndex, column ->
@@ -55,8 +67,10 @@ private fun fillNewColumn(
   elements: List<Element>,
 ): StringResColumn {
   elements.forEach { element ->
+    if (element.name != "string") return@forEach
+    val key = element.getAttributeValue("name") ?: return@forEach
     val stringRes = StringRes(
-      name = element.getAttributeValue("name"),
+      name = key,
       value = element.text,
     )
     column[stringRes.name] = stringRes
