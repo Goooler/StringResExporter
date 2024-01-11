c56d79e7c3d954eaab5f7be48686fb0b6a395099
Goooler <wangzongler@gmail.com>
2024-01-11 09:48:08 +0800

    Generate `xls2res` by Copilot

---
+++ src/main/kotlin/io/github/goooler/exporter/Xls2Res.kt
@@ -0,0 +1,41 @@
+package io.github.goooler.exporter
+
+import org.apache.poi.ss.usermodel.WorkbookFactory
+import org.jdom2.Document
+import org.jdom2.Element
+import org.jdom2.output.Format
+import org.jdom2.output.XMLOutputter
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+fun xls2res(inputPath: String, outputPath: String) {
+  val workbook = WorkbookFactory.create(FileInputStream(File(inputPath)))
+  val sheet = workbook.getSheetAt(0)
+  val stringResMap = mutableMapOf<String, MutableList<StringRes>>()
+
+  sheet.rowIterator().asSequence().drop(1).forEach { row ->
+    val key = row.getCell(0).stringCellValue
+    row.cellIterator().asSequence().drop(1).forEachIndexed { index, cell ->
+      val folderName = sheet.getRow(0).getCell(index + 1).stringCellValue
+      val value = cell.stringCellValue
+      stringResMap.getOrPut(folderName) { mutableListOf() }.add(StringRes(key, value))
+    }
+  }
+
+  stringResMap.forEach { (folderName, stringResList) ->
+    val rootElement = Element("resources")
+    stringResList.forEach { stringRes ->
+      val stringElement = Element("string")
+      stringElement.setAttribute("name", stringRes.name)
+      stringElement.text = stringRes.value
+      rootElement.addContent(stringElement)
+    }
+
+    val document = Document(rootElement)
+    val xmlOutputter = XMLOutputter(Format.getPrettyFormat())
+    val outputFile = File(outputPath, "$folderName/strings.xml")
+    outputFile.parentFile.mkdirs()
+    xmlOutputter.output(document, FileOutputStream(outputFile))
+  }
+}
