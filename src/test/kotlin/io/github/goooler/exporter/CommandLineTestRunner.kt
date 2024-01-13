117f8992ce64317304f536218f7698c35f7d0022
Goooler <wangzongler@gmail.com>
2024-01-13 22:41:35 +0800

    Cleanup CommandLineTestRunner

--- src/test/kotlin/io/github/goooler/exporter/CommandLineTestRunner.kt
+++ src/test/kotlin/io/github/goooler/exporter/CommandLineTestRunner.kt
@@ -1,9 +1,7 @@
 package io.github.goooler.exporter
 
-import java.io.InputStream
-import java.io.Reader
+import java.nio.charset.StandardCharsets
 import java.nio.file.Path
-import java.util.concurrent.TimeUnit
 
 class CommandLineTestRunner(
   private val tempDir: Path,
@@ -16,38 +14,16 @@ class CommandLineTestRunner(
     val process = ProcessBuilder(cliCommand(converter, inputPath, outputPath)).apply {
       directory(tempDir.toFile())
     }.start()
-
-/*    val stdin = InputStream.nullInputStream()
-    process.outputStream.use(stdin::copyTo)*/
-
-    // Give the process some time to complete
-    (0..WAIT_INTERVAL_MAX_OCCURRENCES).forEach { _ ->
-      if (process.isAlive) {
-        // Check regularly whether the ktlint command has finished to speed up the unit testing
-        process.waitFor(WAIT_INTERVAL_DURATION, TimeUnit.MILLISECONDS)
-      }
-    }
-
-    // Get the output from the process before destroying it as otherwise the streams are not collected completely
-    val output = process.inputStream.bufferedReader().use(Reader::readLines)
-    val error = process.errorStream.bufferedReader().use(Reader::readLines)
-
-    if (process.isAlive) {
-      process.destroyForcibly()
-    } else {
-      val exitCode = process.exitValue()
-      if (exitCode != 0) {
-        error.forEach(::println)
-        throw IllegalStateException("Process exited with code $exitCode")
-      }
+    process.waitFor()
+    val err = process.errorStream.readBytes().toString(StandardCharsets.UTF_8)
+//    val out = process.inputStream.readBytes().toString(StandardCharsets.UTF_8)
+    if (err.isNotEmpty()) {
+      error("Error occurred when running command line: $err")
     }
   }
 
   companion object {
-    private const val WAIT_INTERVAL_DURATION = 100L
-    private const val WAIT_INTERVAL_MAX_OCCURRENCES = 10
     private val cliPath = System.getProperty("CLI_PATH") ?: error("CLI_PATH must not be null.")
-    private val isWindows = System.getProperty("os.name").startsWith("Windows")
 
     private fun cliCommand(vararg arguments: String): String = buildList {
       add("java")
