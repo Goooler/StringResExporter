738be3fdc10b1f574547cbc2bb7956f87aff5c44
Zongle Wang <wangzongler@gmail.com>
2024-01-14 18:59:36 +0800

    Publish build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,18 @@ pluginManagement {
 }
 
 plugins {
+  id("com.gradle.enterprise") version "3.16.1"
   id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+val isCI = providers.environmentVariable("CI").isPresent
+
+gradleEnterprise {
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+    publishAlwaysIf(isCI)
+  }
 }
 
 dependencyResolutionManagement {
