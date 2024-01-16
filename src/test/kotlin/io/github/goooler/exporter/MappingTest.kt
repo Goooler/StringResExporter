b791542467c6b76eb2dfc7729b1912fc178afc7a
Goooler <wangzongler@gmail.com>
2024-01-16 22:21:36 +0800

    Test parseString

--- src/test/kotlin/io/github/goooler/exporter/MappingTest.kt
+++ src/test/kotlin/io/github/goooler/exporter/MappingTest.kt
@@ -1,11 +1,26 @@
 package io.github.goooler.exporter
 
 import assertk.assertThat
+import assertk.assertions.containsExactly
 import assertk.assertions.isEqualTo
 import org.jdom2.input.SAXBuilder
 import org.junit.jupiter.api.Test
 
 class MappingTest {
+
+  @Test
+  fun parseString() {
+    val inputStream = requireResourceAsStream("/res/values-it/strings.xml")
+    val stringResList = SAXBuilder().build(inputStream).rootElement.children.mapNotNull {
+      it.toStringResOrNull()
+    }
+    val actual = arrayOf(
+      StringRes("first", "primo"),
+      StringRes("forth", "quarto"),
+      StringRes("seventh", "settimo"),
+    )
+    assertThat(stringResList).containsExactly(*actual)
+  }
 
   @Test
   fun parsePlurals() {
