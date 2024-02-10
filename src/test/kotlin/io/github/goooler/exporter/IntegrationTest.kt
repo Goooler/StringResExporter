10e4f4387fc4244b6f3961d2a0a620f4b9ace25f
Goooler <wangzongler@gmail.com>
2024-02-10 17:51:57 +0800

    Revert "Tweak property refs"
    This reverts commit 4f892de2b34fd671c7b1d7894b51d954a881fdce.

--- src/test/kotlin/io/github/goooler/exporter/IntegrationTest.kt
+++ src/test/kotlin/io/github/goooler/exporter/IntegrationTest.kt
@@ -101,12 +101,10 @@ class IntegrationTest {
 
     assertThat(expected.isNotEmpty()).isEqualTo(true)
     assertThat(actual.isNotEmpty()).isEqualTo(true)
-    ArrayRes::name.let { ref ->
-      assertThat(expected.map(ref)).containsExactly(*actual.map(ref).toTypedArray())
-    }
-    ArrayRes::values.let { ref ->
-      assertThat(expected.flatMap(ref)).containsAtLeast(*actual.flatMap(ref).toTypedArray())
-    }
+    assertThat(expected.map(ArrayRes::name))
+      .containsExactly(*actual.map(ArrayRes::name).toTypedArray())
+    assertThat(expected.flatMap(ArrayRes::values))
+      .containsAtLeast(*actual.flatMap(ArrayRes::values).toTypedArray())
   }
 
   private inline fun <reified T : TranslatableRes> parseRes(resRoot: Path, resFile: String): List<T> {
