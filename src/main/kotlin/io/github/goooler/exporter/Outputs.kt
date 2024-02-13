03e7fa1d471ecce99efa2f66ab797abfe6c62742
Goooler <wangzongler@gmail.com>
2024-02-13 12:57:11 +0800

    Cleanups

--- src/main/kotlin/io/github/goooler/exporter/Outputs.kt
+++ src/main/kotlin/io/github/goooler/exporter/Outputs.kt
@@ -5,16 +5,20 @@ import com.github.ajalt.mordant.terminal.Terminal
 import kotlin.system.exitProcess
 
 internal fun infoOutput(message: Any) {
-  terminal.println(TextColors.green(message.toString()))
+  output(message.toString(), TextColors.green)
 }
 
 internal fun warnOutput(message: Any) {
-  terminal.println(TextColors.yellow(message.toString()))
+  output("Warning: $message", TextColors.yellow)
 }
 
 internal fun errorOutput(message: Any): Nothing {
-  terminal.println(TextColors.red(message.toString()))
+  output("Error: $message", TextColors.red)
   exitProcess(1)
+}
+
+private fun output(message: String, color: TextColors) {
+  terminal.println((color(message)))
 }
 
 private val terminal = Terminal()
